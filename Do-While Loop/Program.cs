58fab3950b98cbed0a4321c09166c40bcec493e6
apesdorzatae <faikha'adzrookaamilaqurrota'aina@bmd.sch.id>
2024-08-20 11:34:04 +0700

    countdown.

---
+++ Do-While Loop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Do_While_Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Percobaan Pertama 
+            int a = 10;
+
+            do
+            {
+                Console.WriteLine("Countdown : " + a);
+                a--;
+            } while (a > -1);
+        }
+    }
+}
